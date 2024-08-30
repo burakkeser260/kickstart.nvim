2c6d9774fc6e0d2383034334ceee46ae0b0d3610
burakkeser260 <91871817+burakkeser260@users.noreply.github.com>
2024-08-30 19:10:19 +0300

    php configs

--- init.lua
+++ init.lua
@@ -617,7 +617,16 @@ require('lazy').setup({
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         --
-
+        intelephense = {
+          ['intelephense.environment.includePaths'] = {
+            '/Users/burakkeser/Jotform/forked/api',
+            '/Users/burakkeser/Jotform/forked/backend',
+            '/Users/burakkeser/Jotform/forked/vendors',
+            '/Users/burakkeser/Jotform/forked/Jotform3',
+            '/Users/burakkeser/Jotform/forked/core',
+          },
+          ['intelephense.format.enable'] = false,
+        },
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
@@ -699,6 +708,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        php = { 'phpcbf' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
@@ -918,18 +928,17 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,20 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'mbbill/undotree',
+  },
+  {
+    'tpope/vim-fugitive',
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+      require('treesitter-context').setup {
+        multiline_threshold = 1,
+        max_lines = 10,
+      }
+    end,
+  },
+}
